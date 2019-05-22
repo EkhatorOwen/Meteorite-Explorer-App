4c2c265c4951b8f5579c044a251795fb844a059e
Owen Ekhator <owenekhator@rocketmail.com>
2019-05-21 23:16:54 -0500

    modified column

---
+++ src/components/table/columns.js
@@ -0,0 +1,51 @@
+export const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name Type',
+    dataIndex: 'nametype',
+    key: 'nametype',
+  },
+  {
+    title: 'Rec Class',
+    dataIndex: 'recclass',
+    key: 'recclass',
+  },
+  {
+    title: 'Mass(g)',
+    dataIndex: 'mass',
+    key: 'mass',
+  },
+  {
+    title: 'Fall',
+    dataIndex: 'fall',
+    key: 'fall',
+  },
+  {
+    title: 'Year',
+    dataIndex: 'year',
+    key: 'year',
+  },
+  {
+    title: 'Latitude',
+    dataIndex: 'latitude',
+    key: 'latitude',
+  },
+  {
+    title: 'Longtitude',
+    dataIndex: 'longtitude',
+    key: 'longtitude',
+  }
+];
+
+export const getData=() =>{
+ 
+}
