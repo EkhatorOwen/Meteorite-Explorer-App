3229aed5c709b5c63b6804bf5167a6ce130ae07e
Owen Ekhator <owenekhator@rocketmail.com>
2019-05-23 19:11:54 -0500

    modified code to receive props via context

--- src/components/table/Resulttable.js
+++ src/components/table/Resulttable.js
@@ -1,47 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, {useContext} from 'react';
 
 import {Table} from 'antd';
 
 import {columns} from './columns';
 
+import {ResultContext} from '../../App';
+
 import './table.css';
 
 const Resulttable = () => {
-
-    const [value, setValue] = useState([])
-
-  useEffect(()=>{
-    fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-     
-      let newArr= []
-      data.sort((a,b)=>a.name>b.name).map(elem=>{
-        let obj = {
-          name: elem.name,
-          id: elem.id,
-          nametype: elem.nametype,
-          recclass: elem.recclass,
-          mass: elem.mass,
-          fall: elem.fall,
-          year: new Date(elem.year).toDateString(),
-           latitude: elem.geolocation ? elem.geolocation.latitude : '',
-           logitude: elem.geolocation ? elem.geolocation.longitude : ''
-        }
-       return newArr.push(obj)
-      })
-      setValue(newArr)
-    });
-  },[])
-
-
+  const value = useContext(ResultContext);
 
   return (
     <div className="result-container">
       <div className="result-table">
-        <Table rowKey={value=>value.id} style={{color:'white'}} columns={columns} pagination={{ pageSize: 10 }} dataSource={value} />
+        {value.loading ? (
+          <p>loading...</p>
+        ) : (
+          <Table
+            rowKey={(value) => value.id}
+            style={{color: 'white'}}
+            columns={columns}
+            pagination={{pageSize: 10}}
+            dataSource={value.value}
+          />
+        )}
       </div>
     </div>
   );
