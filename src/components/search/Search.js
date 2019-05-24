f46cfdbfa16d0ed9b504e3502b581a703d2cd593
Owen Ekhator <owenekhator@rocketmail.com>
2019-05-23 19:10:15 -0500

    added functionality using context

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {useContext, useState}  from 'react';
+import {ResultContext} from '../../App';
 
-import { Input, Button  } from 'antd';
 
-import './search.css'
+import {Input, Button} from 'antd';
+
+import './search.css';
 
 const Search = () => {
+  const [text, setText] = useState('')
+  const value = useContext(ResultContext);
+
   return (
     <div className="search-box">
-      <Input className="search-box-input" placeholder="Enter search terms"/>
-      <Button type="primary">Primary</Button>
-
+      <div className="search-box-input">
+        <Input onChange={e=>setText(e.target.value)} style={{width: '85%'}} placeholder="Enter search terms" />
+        <Button onClick={()=>value.handleSearch(text)}  style={{backgroundColor:"#3F51B5"}} type="primary">Search</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
